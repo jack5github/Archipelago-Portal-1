227a2fbed169e0297bbf078b69de4c1187bc12a8
Jack5 <jack5answers@gmail.com>
2025-11-29 15:11:04 +1100

    Add all radio dinosaur noises as checks

--- worlds/portal1/locations.py
+++ worlds/portal1/locations.py
@@ -5,69 +5,75 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from . import Portal1World
 
+"""
+Location ID glossary:
+
+- 852##: Radio Dinosaur Noise
+"""
+
 LOCATION_NAME_TO_ID = {
     "Chamber 00 Flush Toilet": 1,
     "Chamber 00 Detach Security Camera 1": 2,
     "Chamber 00 Detach Security Camera 2": 3,
-    "Chamber 00 Radio Dinosaur Noise": 4,
+    "Chamber 00 Radio Dinosaur Noise": 85200,
     "Chamber 00 Fizzle Radio": 5,
     "Chamber 00 Detach Security Camera 3": 6,
     "Chamber 01 Detach Security Camera": 7,
-    "Chamber 01 Radio Dinosaur Noise": 8,
+    "Chamber 01 Radio Dinosaur Noise": 85201,
     "Chamber 01 Fizzle Radio": 9,
     "Chamber 02 Detach Security Camera 1": 10,
     "Chamber 02 Detach Security Camera 2": 11,
     "Chamber 02 Detach Security Camera 3": 12,
-    "Chamber 02 Radio Dinosaur Noise": 13,
+    "Chamber 02 Radio Dinosaur Noise": 85202,
     "Chamber 02 Fizzle Radio": 14,
     "Chamber 03 Detach Security Camera 1": 15,
     "Chamber 03 Detach Security Camera 2": 16,
     "Chamber 03 Detach Security Camera 3": 17,
-    "Chamber 03 Radio Dinosaur Noise": 18,
+    "Chamber 03 Radio Dinosaur Noise": 85203,
     "Chamber 03 Fizzle Radio": 19,
     "Chamber 04 Detach Security Camera 1": 20,
-    "Chamber 04 Radio Dinosaur Noise": 21,
+    "Chamber 04 Radio Dinosaur Noise": 85204,
     "Chamber 04 Fizzle Radio": 22,
     "Chamber 04 Detach Security Camera 2": 23,
     "Chamber 05 Detach Security Camera 1": 24,
     "Chamber 05 Detach Security Camera 2": 25,
     "Chamber 05 Detach Security Camera 3": 26,
-    "Chamber 05 Radio Dinosaur Noise": 27,
+    "Chamber 05 Radio Dinosaur Noise": 85205,
     "Chamber 05 Fizzle Radio": 28,
-    "Chamber 06 Radio Dinosaur Noise": 29,
+    "Chamber 06 Radio Dinosaur Noise": 85206,
     "Chamber 06 Fizzle Radio": 30,
-    "Chamber 07 Radio Dinosaur Noise": 31,
+    "Chamber 07 Radio Dinosaur Noise": 85207,
     "Chamber 07 Fizzle Radio": 32,
-    "Chamber 08 Radio Dinosaur Noise": 33,
+    "Chamber 08 Radio Dinosaur Noise": 85208,
     "Chamber 08 Fizzle Radio": 34,
-    "Chamber 09 Radio Dinosaur Noise": 35,
+    "Chamber 09 Radio Dinosaur Noise": 85209,
     "Chamber 09 Fizzle Radio": 36,
     "Chamber 10 Detach Security Camera": 37,
-    "Chamber 10 Radio Dinosaur Noise": 38,
+    "Chamber 10 Radio Dinosaur Noise": 85210,
     "Chamber 10 Fizzle Radio": 39,
     "Chamber 11 Detach Security Camera": 40,
-    "Chamber 11 Radio Dinosaur Noise": 41,
+    "Chamber 11 Radio Dinosaur Noise": 85211,
     "Chamber 11 Fizzle Radio": 42,
-    "Chamber 12 Radio Dinosaur Noise": 43,
+    "Chamber 12 Radio Dinosaur Noise": 85212,
     "Chamber 12 Fizzle Radio": 44,
     "Chamber 13 Detach Security Camera 1": 45,
     "Chamber 13 Detach Security Camera 2": 46,
-    "Chamber 13 Radio Dinosaur Noise": 47,
+    "Chamber 13 Radio Dinosaur Noise": 85213,
     "Chamber 13 Fizzle Radio": 48,
     "Chamber 13 Detach Security Camera 3": 49,
     "Chamber 13 (Advanced) Detach Security Camera 1": 50,
     "Chamber 13 (Advanced) Detach Security Camera 2": 51,
     "Chamber 13 (Advanced) Detach Security Camera 3": 52,
-    "Chamber 14 Radio Dinosaur Noise": 53,
+    "Chamber 14 Radio Dinosaur Noise": 85214,
     "Chamber 14 Fizzle Radio": 54,
     "Chamber 15 Detach Security Camera 1": 55,
-    "Chamber 15 Radio 1 Dinosaur Noise": 56,
+    "Chamber 15 Radio 1 Dinosaur Noise": 85215,
     "Chamber 15 Fizzle Radio 1": 57,
     "Chamber 15 Detach Security Camera 2": 58,
     "Chamber 15 Detach Security Camera 3": 59,
     "Chamber 15 Detach Security Camera 4": 60,
     "Chamber 15 Detach Security Camera 5": 61,
-    "Chamber 15 Radio 2 Dinosaur Noise": 62,
+    "Chamber 15 Radio 2 Dinosaur Noise": 85216,
     "Chamber 15 Fizzle Radio 2": 63,
     "Chamber 15 (Advanced) Detach Security Camera 1": 64,
     "Chamber 15 (Advanced) Detach Security Camera 2": 65,
@@ -77,7 +83,7 @@ LOCATION_NAME_TO_ID = {
     "Chamber 16 Detach Security Camera 1": 69,
     "Chamber 16 Detach Security Camera 2": 70,
     "Chamber 16 Detach Security Camera 3": 71,
-    "Chamber 16 Radio Dinosaur Noise": 72,
+    "Chamber 16 Radio Dinosaur Noise": 85217,
     "Chamber 16 Fizzle Radio": 73,
     "Chamber 16 Detach Security Camera 4": 74,
     "Chamber 16 Detach Security Camera 5": 75,
@@ -88,19 +94,25 @@ LOCATION_NAME_TO_ID = {
     "Chamber 16 (Advanced) Detach Security Camera 5": 80,
     "Chamber 17 Detach Security Camera 1": 81,
     "Chamber 17 Detach Security Camera 2": 82,
-    "Chamber 17 Radio Dinosaur Noise": 83,
+    "Chamber 17 Radio Dinosaur Noise": 85218,
     "Chamber 17 Fizzle Radio": 84,
     "Chamber 17 (Advanced) Detach Security Camera 1": 85,
     "Chamber 17 (Advanced) Detach Security Camera 2": 86,
-    "Chamber 18 Radio Dinosaur Noise": 87,
-    "Chamber 18 Fizzle Radio": 88,
+    "Chamber 18 Radio 1 Dinosaur Noise": 85219,
+    "Chamber 18 Fizzle Radio 1": 88,
     "Chamber 18 Detach Security Camera 1": 89,
     "Chamber 18 Detach Security Camera 2": 90,
+    "Chamber 18 Radio 2 Dinosaur Noise": 85220,
+    "Chamber 18 Radio 3 Dinosaur Noise": 85221,
     "Chamber 18 (Advanced) Detach Security Camera 1": 91,
     "Chamber 18 (Advanced) Detach Security Camera 2": 92,
     "Chamber 19 Detach Security Camera 1": 93,
     "Chamber 19 Detach Security Camera 2": 94,
     "Chamber 19 Detach Security Camera 3": 95,
+    "Escape Part 1 Radio Dinosaur Noise": 85222,
+    "Escape Part 2 Radio Dinosaur Noise": 85223,
+    "Escape Part 3 Radio Dinosaur Noise": 85224,
+    "Escape Part 4 Radio Dinosaur Noise": 85225,
 }
 
 
@@ -121,7 +133,7 @@ def create_all_locations(world: Portal1World) -> None:
 
 
 def create_regular_locations(world: Portal1World) -> None:
-    from .options import AdvancedChambers, EscapeSequence
+    from .options import AdvancedChambers, DinosaurHunt, EscapeSequence, Radios
 
     testchmb_a_00 = world.get_region("Chambers 00-01")
     testchmb_a_01 = world.get_region("Chambers 02-03")
@@ -149,6 +161,11 @@ def create_regular_locations(world: Portal1World) -> None:
     escape_02 = world.get_region("Escape Part 4 & GLaDOS' Chamber")
 
     for k in LOCATION_NAME_TO_ID.keys():
+        if ("Radio" in k and world.options.radios == Radios.option_false) or (
+            "Dinosaur Noise" in k
+            and world.options.dinosaur_hunt == DinosaurHunt.option_disabled
+        ):
+            continue
         if "Chamber 00" in k or "Chamber 01" in k:
             testchmb_a_00.locations.append(
                 Portal1Location(
